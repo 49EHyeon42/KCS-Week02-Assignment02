0e9c05cae1ee4aaee657cf9098975972425d246a
49EHyeon42 <49ehyeon42@gmail.com>
2024-04-27 10:00:35 +0900

    temp: post의 제목, 작성자 이미지, 작성자 이름, 생성일자 fetch로 변경

--- public/post.js
+++ public/post.js
@@ -11,9 +11,9 @@ document.querySelector('.profile-image').addEventListener('click', () => {
 });
 
 // 게시글 수정 버튼 로직
-document.querySelector('.post-update-button').addEventListener('click', () => {
-  window.location.href = '/update-post';
-});
+// document.getElementById('post-update-button').addEventListener('click', () => {
+//   window.location.href = '/update-post';
+// });
 
 // 조회수, 댓글수에 따라 텍스트 변경 로직
 function changeTextToInnerText(text) {
@@ -47,14 +47,14 @@ commentInput.addEventListener('change', () => {
 });
 
 // 게시글 삭제 버튼 관련 로직
-const postDeleteModal = document.getElementById('post-delete-modal');
+// const postDeleteModal = document.getElementById('post-delete-modal');
 
-document.getElementById('post-delete-button').addEventListener('click', () => {
-  // 스크롤 방지
-  document.body.classList.add('stop-scroll');
+// document.getElementById('post-delete-button').addEventListener('click', () => {
+//   // 스크롤 방지
+//   document.body.classList.add('stop-scroll');
 
-  postDeleteModal.style.display = 'flex';
-});
+//   postDeleteModal.style.display = 'flex';
+// });
 
 // 게시글 삭제 버튼, 취소
 document
@@ -115,3 +115,58 @@ document
 
     commentDeleteModal.style.display = 'none';
   });
+
+const postId = parseInt(window.location.href.match(/\/(\d+)$/)[1]);
+const postHeader = document.getElementById('post-header');
+const postInfoContainer = document
+
+fetch('/json/posts.json')
+.then(response => {
+  if (!response.ok) {
+    throw new Error();
+  }
+
+  response.json().then(json =>{
+    const foundPost = json.posts.find(post => post.id === postId);
+
+    if (!foundPost) {
+      throw new Error();
+    }
+
+    const postTitle = document.createElement('p');
+    postTitle.innerText = `${foundPost.title}`;
+    postTitle.id = 'post-title';
+
+    postHeader.appendChild(postTitle);
+
+    const postInfoContainer = document.createElement('div');
+    postInfoContainer.id = 'post-info-container';
+
+    const postAuthorImage = document.createElement('img');
+    postAuthorImage.id = 'post-author-image';
+    postAuthorImage.src = `${foundPost.author.imageUrl}`;
+
+    postInfoContainer.appendChild(postAuthorImage);
+
+    const postAuthorName = document.createElement('span');
+    postAuthorName.id = 'post-author-name';
+    postAuthorName.innerText = `${foundPost.author.name}`;
+
+    postInfoContainer.appendChild(postAuthorName);
+
+    const postCreatedDate = document.createElement('span');
+    postCreatedDate.id = 'post-created-date';
+    postCreatedDate.innerText = `${foundPost.createdDate}`;
+
+    postInfoContainer.appendChild(postCreatedDate);
+
+    // TODO 버튼 다음에
+
+    postHeader.appendChild(postInfoContainer);
+  })
+})
+.catch(error => {
+  alert(error.message);
+
+  console.error(error);
+})
